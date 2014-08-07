e34507a572a07cc07451ecbcf08313e2be004212
Kevin Pham <kevin@MacBook-Air.local>
2014-08-06 17:08:06 -0700

    Initial Commit

---
+++ Assignment 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment 1
+//
+//  Created by Kevin Pham on 8/6/14.
+//  Copyright (c) 2014 Kevin Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
